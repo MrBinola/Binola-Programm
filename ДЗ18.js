0f4795f1653fea03e0e1359f2f2d350717057afc
Code Wizard <codewizard@gmail.com>
2024-05-15 14:24:57 +0300

    Defenders of MrBinola

---
+++ ДЗ18.js
@@ -0,0 +1,27 @@
+let множина = new Set();
+
+множина.add(14);
+множина.add("MrBinola");
+
+console.log(множина)
+
+
+
+const numbers = [7, 10, 18, 19, 23];
+
+const addTwo = (number) => number + 2;
+
+const result = numbers.map(addTwo);
+
+console.log(result);
+
+
+
+let myArray = [];
+
+myArray.push('Deferens2-89');
+
+myArray.push('Defenders of Ukraine');
+
+console.log(myArray);
+
